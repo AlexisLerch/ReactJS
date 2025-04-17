d9b56ab26e86ce562e15477d2103142b191b62ed
Alexis Lerch <alexis.lerch123@gmail.com>
2025-04-17 16:04:44 -0300

    Agregando titulo, background y imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Search from './components/Search'
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <main>
+      <div className='pattern' />
+
+      <div className='wrapper'>
+        <header>
+
+          <img src="./hero.png" alt="Hero banner" />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search />
+      </div>
+    </main>
   )
 }
 
