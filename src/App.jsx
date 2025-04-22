98aae2356aed58e0afd4cd3da0f2dfa7cf2344ff
Alexis Lerch <alexis.lerch123@gmail.com>
2025-04-22 15:37:31 -0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+    'X-Requested-With': 'XMLHttpRequest',
   }
 };
 
